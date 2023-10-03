1097f8dda3cc402c25cab934f6fadcc04b8ee1f5
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-03 22:40:22 +0900

    feat: setup Form Component with react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,26 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormData = {
+  name: string, 
+  price: number,
+  link: string,
+  image: string
+}
+
 const Form = (): JSX.Element => {
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onFormSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data); 
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Item name"/>
-      <input type="text" placeholder="Item price"/>
-      <input type="text" placeholder="Item link"/>
-      <input type="text" placeholder="Item img"/>
+    <form onSubmit={handleSubmit(onFormSubmit)}>
+      <input {...register("name")} type="text" placeholder="Item name"/>
+      <input {...register("price")} type="number" placeholder="Item price"/>
+      <input {...register("link")} type="text" placeholder="Item link"/>
+      <input {...register("image")} type="text" placeholder="Item img"/>
       <button type="submit">Add Item</button>
     </form>
   )
