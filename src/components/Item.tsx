7f9e12d48d540e99ee74e2a316c9cda5a741f969
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-03 10:30:04 +0900

    Create Item interface and create Item components

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,18 @@
-const Item = (): JSX.Element => {
+interface ItemProps {
+  product: string
+  price: number,
+  link: string,
+  img: string
+}
+
+const Item = (props: {data: ItemProps}): JSX.Element => {
+
   return (
     <div>
-      <p>Item title</p>
-      <p>Item price</p>
-      <p>Item link</p>
-      <p>Item image</p>
+      <p>{props.data.product}</p>
+      <p>{props.data.price}</p>
+      <a href={props.data.link} style={{display: "block"}}target="_blank">Link to product</a>
+      <img style={{width: "300px"}}src={props.data.img} alt="product image" />
     </div>
   )
 }
