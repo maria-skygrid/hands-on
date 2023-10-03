9eee3bd4d593f27269b1149f9441f7bccd8d3b57
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-03 15:55:02 +0900

    Update props destructuring

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,18 +1,20 @@
 interface ItemProps {
-  product: string
-  price: number,
-  link: string,
-  img: string
+  data: {
+    product: string
+    price: number,
+    link: string,
+    img: string  
+  }
 }
 
-const Item = (props: {data: ItemProps}): JSX.Element => {
-
+const Item = ({data}: ItemProps): JSX.Element => {
+  const { product, price, link, img } = data
   return (
     <div>
-      <p>{props.data.product}</p>
-      <p>{props.data.price}</p>
-      <a href={props.data.link} style={{display: "block"}}target="_blank">Link to product</a>
-      <img style={{width: "300px"}}src={props.data.img} alt="product image" />
+      <p>{product}</p>
+      <p>{price}</p>
+      <a href={link} style={{display: "block"}}target="_blank">Link to product</a>
+      <img style={{width: "300px"}}src={img} alt="product image" />
     </div>
   )
 }
