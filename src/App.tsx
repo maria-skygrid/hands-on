052ff333dcd6acb9dbea9f1a8b035518e1d1dc07
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-04 10:12:40 +0900

    Delete comment

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,8 @@ type Item = {
 
 const App = () => {
   const [items, setItems] = useState<Item[]>([])
-  // const items: Item[] = [
-  //   {
-  //     id: 1, 
-  //     name: "chair", 
-  //     price: 5000, 
-  //     link: "https://kanademono.design/products/dch-k03?variant=32698086096958&gclid=Cj0KCQjw1OmoBhDXARIsAAAYGSH6ENjmpqJrxwaqkErHlx07OnYi6aoS4TtoUkAoNdgOuIT2vU3CHxgaAuPFEALw_wcB", 
-  //     image: "https://kanademono.design/cdn/shop/products/KanademonoSimpleDiningChairNatural_02_1080x.jpg?v=1629339321"
-  //   }, 
-  //   {
-  //     id: 2, 
-  //     name: "coffee filter holder", 
-  //     price: 2.800, 
-  //     link: "https://www.creema.jp/item/15827002/detail", 
-  //     image: "https://c.p02.c4a.im/images/item/15827002/6f7ecd48131d456494d6e3eaf756b53021fdea73ee2ef342dbe5d338efeb5a78?d=583x585"
-  //   }
-  // ]
 
   const addItem = (data: Item) => {
-    console.log(data);
     const newItem = {
       id: items.length + 1,
       name: data.name,
